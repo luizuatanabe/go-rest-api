945c993763d141890e41dc0ee833ce8a1b3771d4
Luiz Uatanabe <luiz.uatnabe@yes.technology>
2022-09-22 11:35:13 -0300

    first-commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/luizuatanabe/api-go-rest
+
+go 1.18
+
+require github.com/gorilla/mux v1.8.0
---
+++ routes/routes.go
@@ -0,0 +1,17 @@
+package routes
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+	"github.com/luizuatanabe/api-go-rest/controllers"
+)
+
+func HandleRequest() {
+	r := mux.NewRouter()
+	r.HandleFunc("/", controllers.Home)
+	r.HandleFunc("/api/personalidades", controllers.TodasPersonalidades)
+	r.HandleFunc("/api/personalidades/{id}", controllers.RetornaUmaPersonalidade)
+	log.Fatal(http.ListenAndServe(":8000", r))
+}
